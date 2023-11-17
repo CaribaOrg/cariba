f1e8340169352e8e3c29ced4a3f3d6cec3801919
elfadili-ae <elfadili.ae@gmail.com>
2023-11-17 13:08:08 +0100

    Setup: env and mysql setups

---
+++ env_setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#setup the virtualenv
+
+pip install flask
+pip install sqlalchemy
+pip install requests
+
+#if not installed
+# sudo apt-get install python3-dev
+# sudo apt-get install libmysqlclient-dev
+# sudo apt-get install zlib1g-dev
+#
+pip3 install mysqlclient
+
+export CARIBA_MYSQL_USER="root"
+export CARIBA_MYSQL_PWD="root"
+export CARIBA_MYSQL_HOST="localhost"
+#this is for the testing db
+export CARIBA_MYSQL_DB="cariba_test_db"
