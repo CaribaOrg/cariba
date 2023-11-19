fac84351c53f37f1519dbd784eefe9ee4666f610
elfadili-ae <elfadili.ae@gmail.com>
2023-11-19 23:59:04 +0100

    Template: added navbar

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./templates/**/*.html",
     "./static/src/**/"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#21A4D8',
+        darkPrimary: '#0D313D',
+      },
+      fontSize: {
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+      },
+    },
   },
   plugins: [],
 }
