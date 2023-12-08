e0b129096a245efb04bcc0ff3162380c5de07b99
elfadili-ae <elfadili.ae@gmail.com>
2023-12-08 12:19:45 +0100

    Fix: carousel fast forward and overlap when transition

--- app/static/scripts/carousel.js
+++ app/static/scripts/carousel.js
@@ -1,57 +1,74 @@
 var cont = 0;
-let xx;
+var intervalId;
+
 function loopSlider() {
-    xx = setInterval(function () {
+    intervalId = setInterval(function () {
         switch (cont) {
-            case 0: {
-                $("#slider-1").fadeOut(400);
-                $("#slider-2").delay(400).fadeIn(400);
-                $("#sButton1").removeClass("bg-primary");
-                $("#sButton2").addClass("bg-primary");
+            case 0:
+                transitionSlides("#slider-1", "#slider-2", "#sButton1", "#sButton2");
                 cont = 1;
-
                 break;
-            }
-            case 1: {
-                $("#slider-2").fadeOut(400);
-                $("#slider-1").delay(400).fadeIn(400);
-                $("#sButton2").removeClass("bg-primary");
-                $("#sButton1").addClass("bg-primary");
-
+            case 1:
+                transitionSlides("#slider-2", "#slider-1", "#sButton2", "#sButton1");
                 cont = 0;
-
                 break;
-            }
         }
     }, 8000);
 }
 
-function reinitLoop(time) {
-    clearInterval(xx);
-    setTimeout(loopSlider(), time);
+function transitionSlides(fadeOutElem, fadeInElem, removeClassBtn, addClassBtn) {
+    $(fadeOutElem).fadeOut(400, function () {
+        // Hide the outgoing slide after the fadeOut animation
+        $(this).hide();
+    });
+    $(fadeInElem).delay(400).fadeIn(400);
+    $(removeClassBtn).removeClass("bg-primary");
+    $(addClassBtn).addClass("bg-primary");
 }
 
-function sliderButton1() {
-    $("#slider-2").fadeOut(400);
-    $("#slider-1").delay(400).fadeIn(400);
-    $("#sButton2").removeClass("bg-primary");
-    $("#sButton1").addClass("bg-primary");
-    reinitLoop(4000);
-    cont = 0;
+function stopSlider() {
+    clearInterval(intervalId);
 }
 
-function sliderButton2() {
-    $("#slider-1").fadeOut(400);
-    $("#slider-2").delay(400).fadeIn(400);
-    $("#sButton1").removeClass("bg-primary");
-    $("#sButton2").addClass("bg-primary");
-    reinitLoop(4000);
-    cont = 1;
+function restartSlider() {
+    stopSlider();
+    loopSlider();
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function sliderButton(index) {
+    if (index === 0 && cont !== 0) {
+        transitionSlides("#slider-2", "#slider-1", "#sButton2", "#sButton1");
+        cont = 0;
+        restartSlider();
+    } else if (index === 1 && cont !== 1) {
+        transitionSlides("#slider-1", "#slider-2", "#sButton1", "#sButton2");
+        cont = 1;
+        restartSlider();
+    }
+}
+
+$(document).ready(function () {
     $("#slider-2").hide();
     $("#sButton1").addClass("bg-primary");
 
     loopSlider();
+
+    // Pause carousel when the tab is not visible
+    $(document).on("visibilitychange", function () {
+        if (document.hidden) {
+            stopSlider();
+        } else {
+            restartSlider();
+        }
+    });
+
+    // Restart the slider when the page gains focus
+    $(window).on("focus", function () {
+        restartSlider();
+    });
+
+    // Pause the slider when the page loses focus
+    $(window).on("blur", function () {
+        stopSlider();
+    });
 });
